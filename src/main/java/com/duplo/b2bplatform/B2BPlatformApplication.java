e4ad51a68d6fca3f1c10b904076a36dd44a42bcd
Bolaji <bolaji.salau@gmail.com>
2023-08-19 05:16:00 +0100

    Project setup

---
+++ src/main/java/com/duplo/b2bplatform/B2BPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.duplo.b2bplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2BPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2BPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duplo/b2bplatform/B2BPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duplo.b2bplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2BPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
